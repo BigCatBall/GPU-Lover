d6ddde24b118ad44fd35486fda349218e453991e
katsunaa <eugenechung2001@gmail.com>
2021-08-26 06:07:35 +1000

    Added Shoot Animation and Lock Cursor

--- GPU-Lover/Assets/Scripts/Player/ThirdPersonMovement.cs
+++ GPU-Lover/Assets/Scripts/Player/ThirdPersonMovement.cs
@@ -6,20 +6,47 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
-
+    public Animator anim;
     public float speed = 6f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        // lock cursor
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        // movement
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f)
+        // detect movement and change state
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        anim.SetBool("isRunning", isWalking);
+
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
@@ -27,6 +54,19 @@ public class ThirdPersonMovement : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        // change state to shoot straight if mouse click while running
+        if(isWalking)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                anim.SetBool("isShootingStraight", true);
+            }
+        }
+        else
+        {
+            anim.SetBool("isShootingStraight", false);
         }
     }
 }
