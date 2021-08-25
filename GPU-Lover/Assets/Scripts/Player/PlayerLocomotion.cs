f560a4c41dc64512938b1e8cbb1f89a6200881fd
katsunaa <eugenechung2001@gmail.com>
2021-08-26 02:44:13 +1000

    Upgrade to URP

---
+++ GPU-Lover/Assets/Scripts/Player/PlayerLocomotion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+    InputManager inputManager;
+
+    Vector3 moveDirection;
+    Transform cameraObject;
+    Rigidbody playerRb;
+
+    public float movementSpeed = 7;
+
+    private void Awake()
+    {
+        inputManager = GetComponent<InputManager>();
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    public void HandleMovement()
+    {
+        // movement input
+        moveDirection = cameraObject.forward * inputManager.verticalInput;
+        moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput;
+        moveDirection.Normalize();
+        moveDirection.y = 0;
+        moveDirection = moveDirection * movementSpeed;
+
+        Vector3 movementVelocity = moveDirection;
+        playerRb.velocity = movementVelocity;
+    }
+
+    public void HandleRotation()
+    {
+        Vector3 targetDirection = Vector3.zero;
+        targetDirection = cameraObject.forward * inputManager.verticalInput;
+        targetDirection = targetDirection + cameraObject.right * inputManager.horizontalInput;
+        targetDirection.Normalize();
+        targetDirection.y = 0;
+    }
+}
